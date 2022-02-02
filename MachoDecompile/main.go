21b548d2c07ad5bb9ca8a54c79bdf847b7ca12dd
Luke Symons <rezkon93@gmail.com>
2022-02-02 15:02:24 +1100

    XPC Stuff

--- MachoDecompile/main.go
+++ MachoDecompile/main.go
@@ -1,6 +1,6 @@
 package main
 
-//These aren't comments they are compiler flags for the c code.. dont remove them.
+
 // #cgo LDFLAGS: -framework CoreFoundation -framework IOKit
 //int iosurface_utils_get_connection(const char* name);
 //int enumerate_clients(const char* name);
@@ -14,10 +14,8 @@ import (
 	"github.com/blacktop/go-macho"
 	"github.com/fatih/color"
 	_ "github.com/mattn/go-sqlite3"
-	"path/filepath"
-
-	"log"
 	"os"
+	"path/filepath"
 )
 
 
@@ -32,6 +30,18 @@ func io_utils(service string){
 
 }
 
+func dump_path(){
+	db, err := sql.Open("sqlite3", "./XPC.db")
+	check(err)
+	rows, _ := db.Query("SELECT * FROM Macho WHERE Protocols LIKE '%NSXPCListener%'  --case-insensitive")
+	var id int
+	var Program string
+	var Protocol string
+	for rows.Next(){
+		rows.Scan(&id, &Program, &Protocol)
+		color.Red(Program)
+	}
+}
 
 func query_xpc(){
 	db, err := sql.Open("sqlite3", "./XPC.db")
@@ -115,14 +125,12 @@ func parse_file_macho(file_path string) int {
 
 	fmt.Printf("\nImported Libraries\n")
 	for _, y := range macho_file.ImportedLibraries(){
-		fmt.Printf("%s\n", y);
+		color.HiWhite("%s\n", y);
 	}
 	fmt.Println("\nSections")
 	for _, y := range macho_file.Sections{
-		fmt.Printf("%s, Address: 0x%d\n", y.Name, y.Addr)
+		fmt.Printf("%s, Address: 0x%02x\n", y.Name, y.Addr)
 	};
-
-
 
 	return 0
 
@@ -143,10 +151,14 @@ func fat_parse(file_path string)(int){
 		color.Green("File:%s\n", file.Name())
 
 		for _, arch := range fat_file.Arches{
+			var ImportedLibs = arch.ImportedLibraries()
+			for _, Imported := range ImportedLibs{
+				color.HiWhite("Imported Libraries: %s", Imported)
+			}
 
 			ObjProtocols, _ := arch.GetObjCProtocols()
 			for _, Protocols := range ObjProtocols{
-				color.Red("[+] Protcol: %s\n", Protocols.Name);
+				color.Red("[+] Protocol: %s\n", Protocols.Name);
 				color.Yellow("[-]Protocols_Dump: %s\n", Protocols.String());
 				sqlite_macho(file.Name(), Protocols.String())
 
@@ -157,9 +169,9 @@ func fat_parse(file_path string)(int){
 				color.HiBlueString("Optional Methods: %s\n", OptMethods);
 
 
-				if(Protocols.Name == ""){
-					color.HiGreen("[+]Found NSXPCListener! \n")
-					color.Cyan("File Location: %s", file.Name())
+			//	if(Protocols.Name == ""){
+			//		color.HiGreen("[+]Found NSXPCListener! \n")
+			//		color.Cyan("File Location: %s", file.Name())
 
 
 
@@ -170,17 +182,20 @@ func fat_parse(file_path string)(int){
 			}
 
 		}
-	}
-
-
-
-
 	return 0
 
 
 
 
+
+
+
+
+
+
 }
+
+
 
 func walk_directory(path string)([]string, error){
 	fileList := make([]string, 0)
@@ -218,11 +233,15 @@ func main() {
 	i := parser.String("i", "iokit scanner", &argparse.Options{Required: false, Help:"Find IOKit services\n"})
 	o := parser.String("o", "open GUI", &argparse.Options{Required: false, Help:"Open GUI\n"});
 	x := parser.String("x", "XPC", &argparse.Options{Required: false, Help: "Dump XPC Protocols\n"})
+	dp := parser.String("p", "Path", &argparse.Options{Required: false, Help: "Dump Paths\n"})
 	err := parser.Parse(os.Args)
 	if err != nil {
 		// In case of error print error and print usage
 		// This can also be done by passing -h or --help flags
 		fmt.Print(parser.Usage(err))
+	}
+	if *dp != ""{
+		dump_path()
 	}
 
 	if *x != ""{
